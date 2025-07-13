94c21172fd367756926e232b29f564b6ca146946
Omer Akram <om26er@genki.com>
2025-07-13 20:25:13 +0500

    implement session-scope ID generator

---
+++ src/idgen.rs
@@ -0,0 +1,30 @@
+use std::sync::Mutex;
+
+const MAX_ID: i64 = 1 << 53;
+
+pub struct SessionScopeIDGenerator {
+    id: Mutex<i64>,
+}
+
+impl SessionScopeIDGenerator {
+    pub fn new() -> Self {
+        Self { id: Mutex::new(0) }
+    }
+
+    pub fn next_id(&self) -> i64 {
+        let mut id = self.id.lock().unwrap();
+
+        if *id == MAX_ID {
+            *id = 0;
+        }
+
+        *id += 1;
+        *id
+    }
+}
+
+impl Default for SessionScopeIDGenerator {
+    fn default() -> Self {
+        Self::new()
+    }
+}
